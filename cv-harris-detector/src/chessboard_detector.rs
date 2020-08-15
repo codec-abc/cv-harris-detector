88eed25a48c4199539dc8beb96e8cbd12707ba16
codec-abc <viot.camille@outlook.com>
2020-08-15 15:47:15 +0200

    more stuff.

--- cv-harris-detector/src/chessboard_detector.rs
+++ cv-harris-detector/src/chessboard_detector.rs
@@ -1,3 +1,23 @@
 // see 
 // https://res.mdpi.com/d_attachment/sensors/sensors-10-02027/article_deploy/sensors-10-02027.pdf
 //http://ir.hfcas.ac.cn:8080/bitstream/334002/31604/1/Automatic%20chessboard%20corner%20detection%20method.pdf
+
+
+pub fn apply_center_symmetry_filter() {
+
+    todo!();
+
+    // see Section 3.5 in Automatic chessboard corner detection method
+}
+
+pub fn apply_neighbor_distance_filter() {
+    todo!();
+
+    // see Section 3.5 in Automatic chessboard corner detection method
+}
+
+pub fn apply_neighbor_angle_filter() {
+    todo!();
+
+    // see Section 3.5 in Automatic chessboard corner detection method
+}
--- cv-harris-detector/src/main.rs
+++ cv-harris-detector/src/main.rs
@@ -6,7 +6,7 @@ use imageproc::{drawing};
 pub fn main_harris() {
     //let image_path = "./cv-harris-detector/test_images/Harris_Detector_Original_Image.jpg";
     //let image_path = "./cv-harris-detector/test_images/fileListImageUnDist.jpg";
-    let image_path = "./cv-harris-detector/test_images/Calibim1.tif";
+    let image_path = "./cv-harris-detector/test_images/bouguet/Image1.tif";
 
     let src_image = image::open(image_path).expect("failed to open image file");
 
@@ -19,14 +19,14 @@ pub fn main_harris() {
     let height = gray_image.height();
 
     let k: f64 = 0.04f64; // Harris detector free parameter. The higher the value the less it detects.
-    let blur = None;//Some(0.5f32); // TODO: fix this. For very low value the image starts to be completely white
+    let blur = Some(0.3f32); // TODO: fix this. For very low value the image starts to be completely white
 
     let harris_result = cv_harris_detector::harris_corner(&gray_image, k, blur);
 
     let harris_normed = cv_harris_detector::min_max_normalize_harris(&harris_result);
 
     let display_over_original_image = false;
-    let threshold = 120;
+    let threshold = 150;
 
     let mut canvas = if display_over_original_image {
         drawing::Blend(src_image.to_rgba())
@@ -34,39 +34,47 @@ pub fn main_harris() {
         drawing::Blend(harris_normed.clone())
     };
 
+    let mut number_of_corner = 0;
     for x in 0..width {
         for y in 0..height {
             let normed = harris_normed[(x, y)][0];
 
             if normed > threshold {
                 drawing::draw_cross_mut(&mut canvas, Rgba([0, 255, 255, 128]), x as i32, y as i32);
+                number_of_corner = number_of_corner + 1;
             }
             
         }
     }
+
+    println!("detected {} corners", number_of_corner);
 
     let out_img = DynamicImage::ImageRgba8(canvas.0);
     imgshow::imgshow(&out_img);
 }
 
 pub fn main_chessboard_detector() {
-    let image_path = "./cv-harris-detector/test_images/Calibim1.tif";
+    let image_path = "./cv-harris-detector/test_images/bouguet/Image1.tif";
     //let image_path = "./cv-harris-detector/test_images/fileListImageUnDist.jpg";
 
     let src_image = image::open(image_path).expect("failed to open image file");
+
+    let f = 0.5f32;
+    let blurred_image = imageproc::filter::gaussian_blur_f32(&src_image.to_rgb(), f);
 
     let gray_image = src_image.to_luma();
     let width = gray_image.width();
     let height = gray_image.height();
 
-    let mut canvas = drawing::Blend(src_image.to_rgba());
+    let mut canvas = drawing::Blend(blurred_image);
 
 
-    let out_img = DynamicImage::ImageRgba8(canvas.0);
+    let out_img = DynamicImage::ImageRgb8(canvas.0);
     imgshow::imgshow(&out_img);
 }
 
 
 pub fn main() {
-    main_harris();
+    //main_harris();
+    main_chessboard_detector();
 }
